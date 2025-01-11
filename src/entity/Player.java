7b91349cc2728f2b7d689c7f06526bdaabcf63af
Dinuth Diruksha <dinuthk69@gmail.com>
2025-01-12 00:27:06 +0530

    no error player can move

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,13 +3,48 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 
 public class Player extends Entity{
     GamePanel gp;
-    KeyHandler keyh;
+    KeyHandler keyH;
 
-    public Player(GamePanel gp,KeyHandler keyh){
+    public Player(GamePanel gp,KeyHandler keyH){
         this.gp = gp;
-        this.keyh = keyh;
+        this.keyH = keyH;
+        setDefaultValues();
+    }
+
+    public void setDefaultValues(){
+        x=100;
+        y=100;
+        speed=4;
+    }
+
+
+
+    public void update(){
+        if(keyH.upPressed == true){
+            y -= speed;
+        }
+        else if(keyH.downPressed == true){
+            y += speed;
+        }
+        else if(keyH.leftPressed == true){
+            x -= speed;
+        }
+        else if(keyH.rightPressed == true){
+            x += speed;
+        }
+    }
+
+    public void draw(Graphics g2){
+        g2.setColor(Color.white);
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
     }
 }
