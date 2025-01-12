5b9003f4816f5e16a37b772c507d88ecd014e7a8
Dinuth Diruksha <dinuthk69@gmail.com>
2025-01-12 16:44:55 +0530

    done

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -6,7 +6,7 @@ public class Entity {
     public int x,y;
     public int speed;
 
-//    public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
-//    public String direction;
+    public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
+    public String direction;
 
 }
