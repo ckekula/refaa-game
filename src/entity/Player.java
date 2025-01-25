6ee6001f844a47b38a54e68bca5921128bac939f
ckekula <chamukeku@gmail.com>
2025-01-25 17:02:00 +0530

    display chracter at center

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,17 +14,23 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp,KeyHandler keyH){
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -47,19 +53,19 @@ public class Player extends Entity{
     public void update() {
         if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
             if(keyH.upPressed){
-                y -= speed;
+                worldY -= speed;
                 direction = "up";
             }
             else if(keyH.downPressed){
-                y += speed;
+                worldY += speed;
                 direction = "down";
             }
             else if(keyH.leftPressed){
-                x -= speed;
+                worldX -= speed;
                 direction = "left";
             }
             else if(keyH.rightPressed){
-                x += speed;
+                worldX += speed;
                 direction = "right";
             }
 
@@ -113,6 +119,6 @@ public class Player extends Entity{
                 break;
         }
 
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
